837d514e42b4e93729f26aa0687809e8ff9d9f9b
Štefan Barát <quersondc@gmail.com>
2025-03-03 23:40:37 +0100

    hotfix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,11 +8,13 @@ version '1.0.0'
 
 ui_page 'web/build/index.html'
 
+shared_scripts {
+  '@ox_lib/init.lua'
+}
+
 client_scripts {
     'client/*.lua',
 }
-
-
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
 	'server/*.lua'
